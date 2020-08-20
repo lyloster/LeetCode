2807a6cdff02b561d581b132db3f3f5bbc963c65
Kristina Ilyovska <lyloster@gmail.com>
2020-08-20 14:26:51 -0400

    Climbing stairs

---
+++ LeetCode_August/src/solutions/ClimbStairs.java
@@ -0,0 +1,53 @@
+package solutions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * You are climbing a stair case. It takes n steps to reach to the top.
+ *
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+ *
+ * Example 1:
+ *
+ * Input: 2
+ * Output: 2
+ * Explanation: There are two ways to climb to the top.
+ * 1. 1 step + 1 step
+ * 2. 2 steps
+ * Example 2:
+ *
+ * Input: 3
+ * Output: 3
+ * Explanation: There are three ways to climb to the top.
+ * 1. 1 step + 1 step + 1 step
+ * 2. 1 step + 2 steps
+ * 3. 2 steps + 1 step
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= n <= 45
+ * */
+
+public class ClimbStairs {
+    private static final Map<Integer, Integer> store = new HashMap<>();
+
+    public int climbStairs(int n) { //similar to Fibonacci
+        store.put(1,1);
+        store.put(2,2);
+        int ways = climbStairsInternal(n, store);
+        return ways;
+    }
+
+    private int climbStairsInternal(int n, Map<Integer, Integer> store) {
+
+        if (store.containsKey(n)) {
+            return store.get(n);
+        }
+
+        int result = climbStairsInternal(n-1, store) + climbStairsInternal(n-2, store);
+        store.put(n, result);
+        return result;
+    }
+}
